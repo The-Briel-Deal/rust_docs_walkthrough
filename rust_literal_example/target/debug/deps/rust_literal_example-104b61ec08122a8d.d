01f43c6996470db3e5ebd3c3a9d1ba9db99e268a
The-Briel-Deal <gabe@fordltc.net>
2022-05-31 21:31:37 -0400

    rust literal by example

---
+++ rust_literal_example/target/debug/deps/rust_literal_example-104b61ec08122a8d.d
@@ -0,0 +1,5 @@
+/Users/gabrielford/code/rust-docs/rust_literal_example/target/debug/deps/rust_literal_example-104b61ec08122a8d: src/main.rs
+
+/Users/gabrielford/code/rust-docs/rust_literal_example/target/debug/deps/rust_literal_example-104b61ec08122a8d.d: src/main.rs
+
+src/main.rs:
---
+++ rust_literal_example/target/debug/rust_literal_example.d
@@ -0,0 +1,1 @@
+/Users/gabrielford/code/rust-docs/rust_literal_example/target/debug/rust_literal_example: /Users/gabrielford/code/rust-docs/rust_literal_example/src/main.rs
